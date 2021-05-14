da2797bd6fa47610349b2b82ffaa31ecdb3c931a
Eric T <SimplyEr1c@protonmail.com>
2021-05-14 15:01:09 -0400

    Color to Hyperlinks

--- src/components/bottombar.js
+++ src/components/bottombar.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 function BottomBar() {
     return (
-
         <div className="container text-center mx-auto sm:w-auto">
-            <p>Made with <a href="https://reactjs.org/" target='_blank' rel="noreferrer" className="underline">React</a>, and <a href="https://tailwindcss.com/" target='_blank' rel="noreferrer" className="underline">Tailwind CSS</a></p>
+            <p>Made with <a href="https://reactjs.org/" target='_blank' rel="noreferrer" className="underline text-blue-200">React</a>, and <a href="https://tailwindcss.com/" target='_blank' rel="noreferrer" className="underline text-blue-200">Tailwind CSS</a>. <a href="https://github.com/SimplyEr1c/simplyer1cportfolio" target='_blank' rel="noreferrer" className="underline ml-2 text-blue-200">Source Code</a>.</p>
         </div>
     )
 
